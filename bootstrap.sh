8a1d9d50f667b0d9e80cee9f53c6d12a7b531109
Anders Kofoed <anders.kfd@gmail.com>
2018-03-28 18:47:17 +0200

    Add bootstrapper script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ~/.vim
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+mkdir -p ~/.vim/_backup
+mkdir -p ~/.vim/_temp
+ln -s ~/.vim/configs/.vimrc ~/.vimrc
