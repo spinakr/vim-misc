05f2a52e42f8d36286f0df8dd51f673b14cdaaf3
Anders Kofoed <anders.kofoed@if.no>
2018-04-03 10:24:32 +0200

    Make boostrapper work for windows

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -7,3 +7,5 @@ mkdir -p ~/.vim/_backup
 mkdir -p ~/.vim/_temp
 New-Item -ItemType SymbolicLink -Path ~ -Name .vimrc -Value ~/.vim/configs/.vimrc
 cd ~/.vim/bundle/vim-prettier; yarn; cd ../../
+mkdir ~/vimfiles
+New-Item -ItemType SymbolicLink -Path ~/vimfiles -Name bundle -Value ~/.vim/bundle/
